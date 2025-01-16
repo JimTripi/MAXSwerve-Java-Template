b643192675f0e3d16612f3c1454512289f777436
Paolo Calvacca <calvacca.paolo@gmail.com>
2025-01-15 19:02:53 -0500

    Changed turning PID config values
    The reason for the motors spinning randomly out of nowhere was b/c of the
    turningConfig.closedLoop.pid values

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -11,7 +11,7 @@ public final class Configs {
         public static final SparkFlexConfig drivingConfig = new SparkFlexConfig();
         public static final SparkFlexConfig turningConfig = new SparkFlexConfig();
 
-        static {
+         static {
             // Use module constants to calculate conversion factors and feed forward gain.
             double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI
                     / ModuleConstants.kDrivingMotorReduction;
@@ -21,13 +21,13 @@ public final class Configs {
             drivingConfig
                     .idleMode(IdleMode.kBrake)
                     .smartCurrentLimit(50);
-            drivingConfig.encoder
+        drivingConfig.encoder
                     .positionConversionFactor(drivingFactor) // meters
                     .velocityConversionFactor(drivingFactor / 60.0); // meters per second
             drivingConfig.closedLoop
                     .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                     // These are example gains you may need to them for your own robot!
-                    .pid(0.04, 0, 0)
+                    .pid(0.01, 0, 0)
                     .velocityFF(drivingVelocityFeedForward)
                     .outputRange(-1, 1);
 
@@ -43,7 +43,7 @@ public final class Configs {
             turningConfig.closedLoop
                     .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
                     // These are example gains you may need to them for your own robot!
-                    .pid(1, 0, 0)
+                    .pid(0.1, 0, 0)
                     .outputRange(-1, 1)
                     // Enable PID wrap around for the turning motor. This will allow the PID
                     // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
@@ -51,6 +51,10 @@ public final class Configs {
                     // longer route.
                     .positionWrappingEnabled(true)
                     .positionWrappingInputRange(0, turningFactor);
+
+                    
         }
+    
+        
     }
 }
